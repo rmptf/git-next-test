0a6c413f6010d9513151abf712e4be54cc3e8f31
rmptf <rmpotthoff@example.com>
2022-09-25 15:31:59 -0400

    displaying data

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,32 @@
-export default function Home() {
+import { useState, useEffect } from 'react'
 
-  // const dataMain = async () => {
-  //   const response = await fetch('/api/get-data')
-  //   const data = await response.json()
-  //   // console.log(data)
-  //   return data
-  // }
-  // dataMain()
+export default function Home() {
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    fetch('/api/get-data')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [])
+
+  if (isLoading) return <p>Loading...</p>
+  if (!data) return <p>No profile data</p>
 
   return (
     <div>
-      <h1>New Project Hello User</h1>
+      <h1>{data.data}</h1>
+      {/* {comments.map(comment => {
+          return (
+              <div key={comment.id}>
+                  {comment.id}. {comment.text}
+              </div>
+          )
+      })} */}
     </div>
   )
 }
